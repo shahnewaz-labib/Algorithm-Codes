e3ecf7b49821387c79efc340d22acec1c23526f0
shahnewaz-labib <labib.bzs@gmail.com>
2020-09-19 23:01:43 +0600

    added iterative nCr

--- dp-combination-nCr.cpp
+++ dp-combination-nCr.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
+// Iterative method
 long long dp[101][101];
 
-void lmao() {
+void nCr() {
     dp[1][1] = 1;
     dp[1][0] = 1;
     for(int i = 2; i <= 100; i++) {
@@ -18,11 +19,34 @@ void lmao() {
     }
 }
 
+
+// Recursive method
+long long arr[101][101];
+
+int GETnCr(int n, int r) {
+    if(arr[n][r] != 0) return arr[n][r];
+    long long result;
+    if(n == r) result = 1;
+    else if(r == 1) result = n;
+    else if(r == 0) result = 1;
+    else result = GETnCr(n-1, r) + GETnCr(n-1, r-1);
+
+    dp[n][r] = result;
+
+    return dp[n][r];
+
+}
+
 int main() {
-    lmao();
+    /* Approach 1
+    nCr();
     int n, r;
     while(cin >> n >> r) {
         if(!n and !r) return 0;
         printf("%d things taken %d at a time is %lld exactly.\n", n, r, dp[n][r]);
     }
+    */
+
+    // Approach 2
+    cout << GETnCr(50, 3) << endl;
 }
